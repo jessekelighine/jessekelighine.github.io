c4703c623255478bde39159df1dc6b780d2a65cf
Jesse C. Chen <jcc@jessekelighine.com>
2024-09-02 17:24:10 -0400

    update

---
+++ share/generate-redirection.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+###############################################################################
+# -*- encoding: UTF-8 -*-                                                     #
+# Author: Jesse C. Chen (https://jessekelighine.com)                          #
+# Description: Generate HTML file for redirection.                            #
+#                                                                             #
+# Last Modified: 2024-09-01                                                   #
+###############################################################################
+
+output="index.html"
+placeholder="THIS_IS_A_PLACEHOLDER_THAT_CONTAINS_NO_SPECIAL_CHARACTERS"
+template=$(cat << EOF
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta http-equiv="refresh" content="0; url=${placeholder}" />
+		<link rel="stylesheet" href="style.css" />
+	</head>
+	<body>
+		<p>
+			<strong>Note:</strong>
+			You should be redirected automatically.
+			Or click <a href="${placeholder}"><code>here</code></a>.
+		</p>
+	</body>
+</html>
+EOF
+)
+
+[ $# -eq 0 ] && {
+	echo "insufficient arguments"
+	echo "usage: $0 [PATH/URL]"
+	exit 1
+}
+
+echo "${template}" > "${output}"
+sed -i '' "s?${placeholder}?${1//\?/\\?}?g" "${output}"
+exit 0
